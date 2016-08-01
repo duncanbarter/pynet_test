4f40f55456994f11b93ad98000a101fd0cd99d03
Duncan Barter <nullptr@tsumaranai.net>
2016-08-01 13:24:56 -0700

    day 01 exercise 03

---
+++ e01-03.py
@@ -0,0 +1,8 @@
+_first = 'Duncan'
+_mid   = 'McIntyre'
+_last  = 'Barter'
+
+final  = raw_input('Please enter a valid name suffix: ')
+
+print('{:>30}{:>30}{:>30}{:>30}'.format(_first, _mid, _last, final))
+
