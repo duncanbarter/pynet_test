182581a61f178293870ab70659c99b837f4a822b
Duncan Barter <nullptr@tsumaranai.net>
2016-08-02 10:00:02 -0700

    day 02 exercise 14

---
+++ e02-14.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+net_devices = {}
+
+net_devices['ip_addr'] = '10.10.1.8'
+net_devices['username'] = 'duncanb'
+net_devices['password'] = 'ajablah'
+net_devices['vendor'] = 'Cisco'
+net_devices['model'] = '4948 10GE'
+
+for k, v in net_devices.items():
+    print k, v
+
+net_devices['password'] = 'blahaja'
+net_devices['secret'] = 'enabled!!'
+
+_ = net_devices.get('os', 'cisco_ios')
+print _
