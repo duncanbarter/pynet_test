e6ab8ce97b8e7ac22df85e6f844dcdcce25500fb
Duncan Barter <nullptr@tsumaranai.net>
2016-08-02 10:23:20 -0700

    day 01 exercise 09

--- e01-07.py
+++ e01-07.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
+your_ip = raw_input('I need your IP!  Be honest: ')
+octets  = your_ip.split(".")
 
+octets[-1] = '0'
+
+bin_octets = [bin(int(x)) for x in octets(x)]
+
+print('The octets of your IP are:\n{:12}{:12}{:12}{:12}\nSee, that wasn''t so scary!'.format(*octets))
+print('And in binary!')
+print('{}{}{}{}'.format(*bin_octets))
