0e66f093b9f7d0daa7d65076622478dee0b20055
Duncan Barter <nullptr@tsumaranai.net>
2016-08-03 14:31:47 -0700

    day 03 exercise 26

--- e03-26.py
+++ e03-26.py
@@ -2,6 +2,9 @@
 
 from netmiko import ConnectHandler
 from getpass import getpass
+from ciscoconfparse import CiscoConfParse
+
+import sys
 
 def save_file(filename, data):
     with open(filename, "w") as f:
@@ -31,8 +34,40 @@ def main():
         show_ver = sw_con.send_command("show version")
         show_run = sw_con.send_command("show run")
 
+        print '#' * 60
+        print 'VERSION INFO   - ' + sw_con.base_prompt + '\n'
+        print show_ver
+        print '\n'
+
+        print '#' * 60
+        print 'RUNNING CONFIG - ' + sw_con.base_prompt + '\n'
+        print show_run
+        print '\n'
+
         save_file(sw_con.base_prompt + '-ver', show_ver)
         save_file(sw_con.base_prompt + '-run', show_run)
+
+    sw_con = ConnectHandler(**pynet_sw1)
+
+    config_commands = ['vlan 808', 'name sk1tools',]
+
+    output_embedded = sw_con.send_config_set(config_commands)
+    output_imported = sw_con.send_config_from_file(sw_con.base_prompt + '-config-updates')
+
+    print '#' * 60
+    print 'EMBEDDED COMMAND RESULTS\n'
+    print output_embedded
+    print '\n'
+
+    print '#' * 60
+    print 'IMPORTED COMMAND RESULTS\n'
+    print output_imported
+    print '\n'
+
+    print '#' * 60
+    print 'FINAL VLAN OUTPUT\n'
+    print sw_con.send_command('show vlan')
+    print '\n'
 
 if __name__ == "__main__":
     main()
--- e03-27.py
+++ e03-27.py
@@ -2,6 +2,9 @@
 
 from netmiko import ConnectHandler
 from getpass import getpass
+from ciscoconfparse import CiscoConfParse
+
+import sys
 
 def save_file(filename, data):
     with open(filename, "w") as f:
@@ -25,14 +28,27 @@ def main():
         'password':    sw1_pass,
     }
 
-    for curr_device in (pynet_rtr1, pynet_sw1):
-        sw_con = ConnectHandler(**curr_device)
+    sw_con = ConnectHandler(**pynet_rtr1)
 
-        show_ver = sw_con.send_command("show version")
-        show_run = sw_con.send_command("show run")
+    config_commands = ['vlan 808', 'name sk1tools',]
 
-        save_file(sw_con.base_prompt + '-ver', show_ver)
-        save_file(sw_con.base_prompt + '-run', show_run)
+    output_embedded = sw_con.send_config_set(config_commands)
+    output_imported = sw_con.send_config_from_file(sw_con.base_prompt + '-config-updates')
+
+    print '#' * 60
+    print 'EMBEDDED COMMAND RESULTS\n'
+    print output_embedded
+    print '\n'
+
+    print '#' * 60
+    print 'IMPORTED COMMAND RESULTS\n'
+    print output_imported
+    print '\n'
+
+    print '#' * 60
+    print 'FINAL VLAN OUTPUT\n'
+    print sw_con.send_command('show vlan')
+    print '\n'
 
 if __name__ == "__main__":
     main()
